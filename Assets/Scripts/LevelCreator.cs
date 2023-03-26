0f0a86d1d623ecd2310905ffb8bd36d56a5937b7
wolfheat <mihe82@gmail.com>
2023-03-26 03:31:44 +0200

    Update LevelCreator.cs

--- Assets/Scripts/LevelCreator.cs
+++ Assets/Scripts/LevelCreator.cs
@@ -377,15 +377,33 @@ public class LevelCreator : MonoBehaviour
 			rooms.Add(newRoom);
 		}
 
+		// Separate Rooms Until they dont overlap
+
+		RoomMaker.SetClosestRooms(rooms);
+		RoomMaker.MoveOverlap(rooms);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
 
+		// Generate Map to show it
 		foreach (Room room in rooms)
 		{
 			floorPositions.UnionWith(GetRoomBorders(room));
 		}
 
 
-		// Separate Rooms Until they dont overlap
 
 		// Select X largest rooms as main rooms.
 
@@ -431,6 +449,7 @@ public static class RoomMaker
 	private static int RoomMinSize = 4;
 	private static int RoomMaxSize = 10;
 	private static int RoomMaxDev = 3;
+	private static int MoveStrength = 2;
 
 	public static Room GenerateRandomRoom()
 	{
@@ -438,15 +457,73 @@ public static class RoomMaker
 			new Vector2Int(Random.Range(RoomMinSize,RoomMaxSize), Random.Range(RoomMinSize, RoomMaxSize)), 
 			new Vector2Int(Random.Range(-RoomMaxDev, RoomMaxDev), Random.Range(-RoomMaxDev, RoomMaxDev)));
 	}
+
+	internal static bool Overlap(Room r1, Room r2)
+	{
+		bool overlapX = r1.pos.x <= r2.pos.x+r2.size.x && r1.pos.x+r1.size.x >= r2.pos.x;
+		bool overlapY = r1.pos.y <= r2.pos.y+r2.size.y && r1.pos.y+r1.size.y >= r2.pos.y;
+		return overlapX && overlapY;		
+	}
+	
+	internal static void MoveOverlap(List<Room> rooms)
+	{
+		foreach (var room in rooms)
+		{
+			bool overlap = false;
+			foreach (var roomToCheck in room.closest)
+			{
+				if (Overlap(room, roomToCheck))
+				{
+					overlap = true;
+					break;
+				}
+			}
+			MoveAwayFromClosest(room);
+		}
+	}
+
+	private static void MoveAwayFromClosest(Room room)
+	{
+		Vector2 moveDirection = Vector2.zero;
+		foreach (Room otherRoom in room.closest)
+		{
+			Debug.Log("Room Difference: "+ (room.pos - otherRoom.pos)+" Room1: "+room.pos+" Room2: "+otherRoom.pos);
+			moveDirection += (room.pos - otherRoom.pos);
+		}
+		moveDirection = moveDirection.normalized;
+		Vector2Int moveVector = Vector2Int.RoundToInt(moveDirection * MoveStrength);
+		Debug.Log("MoveVector "+moveVector + "because "+moveDirection);
+		Debug.Log("Moving Room from "+room.pos+" to "+(room.pos + moveVector));
+		room.pos += moveVector;
+	}
+
+	internal static void SetClosestRooms(List<Room> rooms)
+	{
+		foreach (var room in rooms)
+		{
+			List<Room> closestRooms = new List<Room>();
+			foreach (var checkedRoom in rooms)
+			{
+				if (room == checkedRoom) continue;
+				closestRooms.Add(checkedRoom);
+				closestRooms.Sort((r1, r2) => Vector2Int.Distance(room.pos, r1.pos).CompareTo(Vector2Int.Distance(room.pos, r2.pos)));
+			}
+			room.closest = closestRooms;
+		}	
+
+	}
 }
 public class Room
 {
 	public Vector2Int size;
 	public Vector2Int pos;
+	public List<Vector2Int> corners;
+	public List<Room> closest;
 
 	public Room(Vector2Int s, Vector2Int p)
 	{
 		size = s;
 		pos = p;
+		corners = new List<Vector2Int>() { p,p+s-Vector2Int.one,new Vector2Int(p.x+s.x-1,p.y), new Vector2Int(p.x, p.y + s.y-1)};
 	}
 }

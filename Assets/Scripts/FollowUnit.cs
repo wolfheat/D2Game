800bc379ba18e8be2fa0981803d49be369913cf3
wolfheat <mihe82@gmail.com>
2023-04-25 22:34:42 +0200

    Added post processing

---
+++ Assets/Scripts/FollowUnit.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FollowUnit : MonoBehaviour
+{
+    [SerializeField] GameObject unitToFollow;
+    [SerializeField] float heightOffset = 0; 
+	
+	private void Update()
+    {
+        transform.position = unitToFollow.transform.position + Vector3.up*heightOffset;
+    }
+
+}
